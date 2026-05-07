4add17f6943ffe3aba90f2ff56ca18f15e5d10d8
vaibhav-saini-dev <vsaini3940@gmail.com>
2026-05-07 16:36:06 -0400

    Update dropdown width

--- project/app/components/Dropdown.js
+++ project/app/components/Dropdown.js
@@ -36,8 +36,8 @@ export default function Dropdown({ text, options }) {
 
                 {/* Dropdown menu */}
                 {isOpen && (
-                    <div className="origin-top-right absolute
-                                    right-0 mt-2 w-56 rounded-md
+                    <div className="text-center
+                                    mt-2  rounded-md
                                     shadow-lg bg-white ring-1 ring-black
                                     ring-opacity-5 focus:outline-none z-10">
                         <div className="py-1">
