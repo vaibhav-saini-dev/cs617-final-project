f1df23ddd992bcc3707fecd93781bd44b9afb34a
vaibhav-saini-dev <vsaini3940@gmail.com>
2026-05-10 16:57:50 -0400

    Update path for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "",
+  basePath: "/cs617-final-project",
   output: "export",
   reactStrictMode: true,
 };
