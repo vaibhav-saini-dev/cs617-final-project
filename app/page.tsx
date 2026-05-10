6834ead41f62847a99fba7bc67fc7879a4950cac
vaibhav-saini-dev <vsaini3940@gmail.com>
2026-05-10 17:52:40 -0400

    Revert path for GitHub Pages
    Turns out, the paths been working this entire time when testing in a private window.

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const [year, setYear] = useState("Year");
 
   useEffect(() => {
-    fetch("https://vaibhav-saini-dev.github.io/cs617-final-project/data/hospital_data.json")
+    fetch("/cs617-final-project/data/hospital_data.json")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
