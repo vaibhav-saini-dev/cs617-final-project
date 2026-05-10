e0a80d6e10539db2bb5ef45865bf0e1bf4913309
vaibhav-saini-dev <vsaini3940@gmail.com>
2026-05-10 17:20:42 -0400

    Add base path again to fix GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/cs617-final-project",
+  assetPrefix: "/cs617-final-project/",
   images: {
     unoptimized: true,
   },
