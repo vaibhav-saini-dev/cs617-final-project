e2213019833424873c31060ccff5f205a6d96b5f
vaibhav-saini-dev <vsaini3940@gmail.com>
2026-05-10 17:50:15 -0400

    Update path for GitHub Pages

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const [year, setYear] = useState("Year");
 
   useEffect(() => {
-    fetch("/cs617-final-project/data/hospital_data.json")
+    fetch("https://vaibhav-saini-dev.github.io/cs617-final-project/data/hospital_data.json")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
